99d530c055679d40ccfc9f8951e68d7765b1dee2
HetPatel0 <142023310+HetPatel0@users.noreply.github.com>
2023-10-18 18:17:37 +0530

    Add files via upload

--- 16a3.c
+++ 16a3.c
@@ -2,19 +2,23 @@
 
 int main(void) {
   
-int a[4][5],i,j;
-for(i=0;i<a;i++) {
+int a[4][5],i,j,c=1,d=1;
+for(i=0;i<4;i++) {
         for(j=0;j<5;j++){
-            printf("Enter roll no with  marks");
+            printf("Roll no is: %d  ",c);
+            c++;
+            
             scanf("%d",&a[i][j]);
         }
     }
 
     for(i=0;i<4;i++) {
         for(j=0;j<5;j++){
-           printf("%d\t",a[i][j]);
+
+           printf("mark of roll no %d %d\t\n",d,a[i][j]);
+           d++;
 
         }
-        printf("\n");
+        
     }
 }
